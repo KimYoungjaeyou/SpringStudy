e1d94a4d5e08ec9b42b0fdb9c3af9866c1b22aee
KimYoungjaeyou <qoloa0326@naver.com>
2019-07-23 01:00:22 +0900

    test

---
+++ SpringApplication8/src/main/java/com/skcc/SpringApplication8Application.java
@@ -0,0 +1,15 @@
+package com.skcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplication8Application.class, args);
+	}
+	
+	
+
+}
